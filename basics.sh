e67a5617dd7ec5f9163557d8afc3cac0f5bb42c5
mamatha-ravi <mamata.r@gmail.com>
2026-01-28 15:19:59 +0530

    basics2

--- basics.sh
+++ basics.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-name1=$1
-name2=$2
+Today=$(date)
+echo "today date is $Today"
 echo "Please enter ur username:"
 read USER_NAME
 echo "Welcome $USER_NAME"
 echo "enter password for $USER_NAME"
 read -s PASSWORD
-
+ name1=$1  # name1=Sai
+name2=$2
 echo "$name1:: Hello, $name2"
 echo  "$name2:: Hello, $name1"
 echo "$name1:: How r u?"
