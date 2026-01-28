06d1a2858245c36199ad23b6cd7d07f3a48a705d
mamatha-ravi <mamata.r@gmail.com>
2026-01-28 15:28:18 +0530

    basics2

--- basics.sh
+++ basics.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-Today=$(date)
-echo "today date is $Today"
-echo "Please enter ur username:"
-read USER_NAME
-echo "Welcome $USER_NAME"
-echo "enter password for $USER_NAME"
-read -s PASSWORD
- name1=$1  # name1=Sai
-name2=$2
-echo "$name1:: Hello, $name2"
-echo  "$name2:: Hello, $name1"
-echo "$name1:: How r u?"
-echo  "$name2:: I am good, how about you?"
-echo "$name1:: I am fine too."
+Starttime=$(date +%s)
+echo "today date is $Starttime"
+sleep 10
+EndTime=$(date +%s)
+echo "End time is $EndTime"
+TotalTime=$((EndTime-Starttime))
+echo "total time executed is : $TotalTime"
+# echo "Please enter ur username:"
+# read USER_NAME
+# echo "Welcome $USER_NAME"
+# echo "enter password for $USER_NAME"
+# read -s PASSWORD
+#  name1=$1  # name1=Sai
+# name2=$2
+# echo "$name1:: Hello, $name2"
+# echo  "$name2:: Hello, $name1"
+# echo "$name1:: How r u?"
+# echo  "$name2:: I am good, how about you?"
+# echo "$name1:: I am fine too."
