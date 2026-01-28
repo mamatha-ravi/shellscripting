a06a94cef22a753e5dd3200441839683e6eea7b9
mamatha-ravi <mamata.r@gmail.com>
2026-01-28 15:59:45 +0530

    basics2

--- datatypes.sh
+++ datatypes.sh
@@ -1,5 +1,9 @@
 #!/bin/bash\
 num1=$1
 num2=$2
-sum=$($num1+$num2)
+sum=$(($num1+$num2))
 echo "sum of $num1 and $num2 is : $sum"
+
+cities=("Hyd" "Bnglr" "Del")
+echo "cities are: ${$cities[@]}"
+echo "city1 is : ${cities[0]}"
