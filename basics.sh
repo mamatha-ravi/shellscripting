498f03e09a1bf8e6587f17fbe4780ea93ecf926b
mamatha-ravi <mamata.r@gmail.com>
2026-01-28 15:07:43 +0530

    basics2

--- basics.sh
+++ basics.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 name1=$1
 name2=$2
+echo "Please enter ur username:"
+read USER_NAME
+echo "Welcome $USER_NAME"
+echo "enter password for $USER_NAME"
+read -s PASSWORD
+
 echo "$name1:: Hello, $name2"
 echo  "$name2:: Hello, $name1"
 echo "$name1:: How r u?"
