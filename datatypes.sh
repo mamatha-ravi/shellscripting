812f6135e7bbdc0309380366815b7dcc3ae96a6a
mamatha-ravi <mamata.r@gmail.com>
2026-01-28 16:05:29 +0530

    basics2

--- datatypes.sh
+++ datatypes.sh
@@ -1,8 +1,17 @@
 #!/bin/bash\
 num1=$1
 num2=$2
-sum=$((num1+num2))
+
+if($num1>$num2) then
+
+sum=$((num1+num2))    #sum=$(($num1+$num2))  this is also correct
 echo "sum of $num1 and $num2 is : $sum"
+elif($num1==$num2)
+echo "both numbers are eaqual"
+
+else 
+echo "$num2 is greter"
+fi
 
 cities=("Hyd" "Bnglr" "Del")
 echo "cities are: ${cities[@]}"
