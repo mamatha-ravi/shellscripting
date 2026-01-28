606c3fd3613f50ad4d1f32f15a8431b62676b9ec
mamatha-ravi <mamata.r@gmail.com>
2026-01-28 17:05:55 +0530

    basics2

--- Hello.sh
+++ Hello.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "Hello, World!"
-echo "this is me"
+# echo "Hello, World!"
+# echo "this is me"
 
 num=$1
-remiander=($num%2)
-if [ $remainder -ne 0 ] then
+remiander=(($num%2))
+if [ "$remainder" -ne 0 ]; then
 echo "$num is odd number"
 exit 1
 else 
