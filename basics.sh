4f271620642f96fdc486021d48d6e75c4f86fe68
mamatha-ravi <mamata.r@gmail.com>
2026-01-28 14:52:00 +0530

    basics2

---
+++ basics.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+name1="Sai"
+name2="Kiran"
+echo "$name1:: Hello, $name2"
+echo  "$name2:: Hello, $name1"
+echo "$name1:: How r u?"
+echo  "$name2:: I am good, how about you?"
+echo "$name1:: I am fine too."
