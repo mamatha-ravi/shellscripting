1644f3671521c7d2a07726eeae7927b4d50cd712
mamatha-ravi <mamata.r@gmail.com>
2026-01-29 16:13:27 +0530

    basics2

---
+++ installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+Userid=$((id -u))
+id=$?
+Package=$1
+if [ $Userid -ne 0 ]; then
+echo "this is not sudo user"
+exit 1
+fi
+echo "installing nginx"
+dnf install nginx -y
+validate $1
+validate (){
+
+    if [ $id -eq 0 ]; then 
+echo "$1 installation Success"
+else 
+echo "$1 installation failure"
+fi
+}
