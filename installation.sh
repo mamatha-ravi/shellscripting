ff2bfbea2e86ef4419cea63606f77ae291de9ca4
mamatha-ravi <mamata.r@gmail.com>
2026-01-29 18:26:32 +0530

    basics2

--- installation.sh
+++ installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 Userid=$(id -u)
-id=$?
+
 R='\e[31m'
 G='\e[32m'
 Y='\e[33m'
@@ -16,7 +16,7 @@ fi
 mkdir -p $log_folder
 # Package=$1
 validate (){
-    if [ $id -eq 0 ]; then 
+    if [ $1 -eq 0 ]; then 
 echo -e "$Package installation $G Success $N" | tee -a $log_file
 else 
 echo -e "$Package installation $R failure $N" | tee -a $log_file
@@ -26,11 +26,11 @@ fi
 for Package in $@
 do
 dnf list installed $Package &>> $log_file
-if [ $id -eq 0 ];then
+if [ $? -eq 0 ];then
 echo  -e "$Y $Package is already installed $N" | tee -a $log_file
 else
 echo "installing $Package"
 dnf install $Package -y &>> $log_file
-validate $Package
+validate $? $Package
 fi
 done
