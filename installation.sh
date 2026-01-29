ce2612eae502fd7065d48997eaee7d8e649a2dbc
mamatha-ravi <mamata.r@gmail.com>
2026-01-29 17:59:51 +0530

    basics2

--- installation.sh
+++ installation.sh
@@ -10,16 +10,16 @@ N='\e[0m'   # No Color
 log_folder="/var/log/shellscript"
 log_file="/var/log/shellscript/$0.log"
 if [ $Userid -ne 0 ]; then
-echo "$R this is not sudo user $N" | tee -a $log_file
+echo -e "$R this is not sudo user $N" | tee -a $log_file
 exit 1
 fi
 mkdir -p $log_folder
 # Package=$1
 validate (){
     if [ $id -eq 0 ]; then 
-echo "$Package installation Success" | tee -a $log_file
+echo -e "$Package installation $G Success $N" | tee -a $log_file
 else 
-echo "$Package installation failure" | tee -a $log_file
+echo -e "$Package installation $R failure $N" | tee -a $log_file
 fi
 }
 
