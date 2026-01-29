e3106ea27c9b0edd80615055048d8dc0fe7474bd
mamatha-ravi <mamata.r@gmail.com>
2026-01-29 16:22:24 +0530

    basics2

--- installation.sh
+++ installation.sh
@@ -2,13 +2,6 @@
 Userid=$((id -u))
 id=$?
 Package=$1
-if [ $Userid -ne 0 ]; then
-echo "this is not sudo user"
-exit 1
-fi
-echo "installing nginx"
-dnf install $Package -y
-validate $Package
 validate (){
 
     if [ $id -eq 0 ]; then 
@@ -17,3 +10,10 @@ else
 echo "$Package installation failure"
 fi
 }
+if [ $Userid -ne 0 ]; then
+echo "this is not sudo user"
+exit 1
+fi
+echo "installing nginx"
+dnf install $Package -y
+validate $Package
