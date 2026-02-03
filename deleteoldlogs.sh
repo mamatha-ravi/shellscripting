b46d1c6708ec8484e4af717a950d2951984ae743
mamatha-ravi <mamata.r@gmail.com>
2026-02-03 16:28:40 +0530

    Robo

--- deleteoldlogs.sh
+++ deleteoldlogs.sh
@@ -20,7 +20,7 @@ DIR="/home/ec2-user/app-log"
 find "$DIR" -type f -name "*.log" -mtime +7 | while IFS= read -r file
 do
   echo "File found: $file"
-  read -p "Do you want to delete $file? (yes/no): " confirm
+  read -p "Do you want to delete $file? (yes/no): " confirm < /dev/tty
 
   if [ "$confirm" = "yes" ]; then
     rm -f "$file"
