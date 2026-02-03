b2b6ff5ad4e15349b619e75839ae37d01d884f48
mamatha-ravi <mamata.r@gmail.com>
2026-02-03 16:15:36 +0530

    Robo

--- deleteoldlogs.sh
+++ deleteoldlogs.sh
@@ -3,7 +3,7 @@ DIR=" /home/ec2-user/app-log"
 deletefiles=$(find $DIR -type f -name "*.log" -mtime +7 )
 # find /path/to/directory -type f -mtime +7 -delete
 echo "delete files: $deletefiles"
-while IFS= read -r file:
+while IFS= read -r file;
  do
  echo "$file"
  done <<< $deletefiles
