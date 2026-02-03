fe238e948f024cbde4e0c4e66080aa9f62ef76c3
mamatha-ravi <mamata.r@gmail.com>
2026-02-03 16:30:10 +0530

    Robo

--- deleteoldlogs.sh
+++ deleteoldlogs.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
+DIR="/home/ec2-user/app-log"
+
+ Deletefile=$(find "$DIR" -type f -name "*.log" -mtime +7) 
+while IFS= read -r file
+do
+  echo "File found: $file"
+  done <<< $Deletefile
+
 # DIR="/home/ec2-user/app-log"
 
-#  Deletefile=$(find "$DIR" -type f -name "*.log" -mtime +7) 
-# while IFS= read -r file
+# find "$DIR" -type f -name "*.log" -mtime +7 | while IFS= read -r file
 # do
 #   echo "File found: $file"
-#   read -p "Do you want to delete $file? (yes/no):" confirm
+#   read -p "Do you want to delete $file? (yes/no): " confirm < /dev/tty
 
 #   if [ "$confirm" = "yes" ]; then
-#      rm -f "$file"
+#     rm -f "$file"
 #     echo "Deleted $file"
 #   else
 #     echo "Skipped $file"
 #   fi
-# done <<< $Deletefile
-
-DIR="/home/ec2-user/app-log"
-
-find "$DIR" -type f -name "*.log" -mtime +7 | while IFS= read -r file
-do
-  echo "File found: $file"
-  read -p "Do you want to delete $file? (yes/no): " confirm < /dev/tty
-
-  if [ "$confirm" = "yes" ]; then
-    rm -f "$file"
-    echo "Deleted $file"
-  else
-    echo "Skipped $file"
-  fi
-done
+# done
