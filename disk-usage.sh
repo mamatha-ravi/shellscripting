28dbfc4ff5e314051a7997cc25952f6eeab7091a
mamatha-ravi <mamata.r@gmail.com>
2026-02-05 12:03:44 +0530

    basics2

--- disk-usage.sh
+++ disk-usage.sh
@@ -5,6 +5,7 @@ Y='\e[33m'
 B='\e[34m'
 N='\e[0m'   # No Color
 message=""
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 usage_threshold=70
 Disk_usage=$(df -hT | grep -v Filesystem)
@@ -18,3 +19,4 @@ message+="high disk utilization in $partition : $usage% \n"
 fi
 done <<< $Disk_usage
 echo -e "$message"
+sh mail.sh "mamata.r@gmail.com" "High Disk Usage Alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
