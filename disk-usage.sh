db711f0cecd5a20511221e83bbf2fd378de8da0a
mamatha-ravi <mamata.r@gmail.com>
2026-02-05 12:17:09 +0530

    basics2

--- disk-usage.sh
+++ disk-usage.sh
@@ -15,7 +15,7 @@ do
 usage=$(echo $logs | awk '{print $6}' | cut -d "%" -f1)
 partition=$(echo $logs | awk '{print $7}')
 if [ "$usage" -gt "$usage_threshold" ]; then
-message+="high disk utilization in $partition : $usage% \n"
+message+="high disk utilization in $partition : $usage% <br>"
 fi
 done <<< $Disk_usage
 echo -e "$message"
